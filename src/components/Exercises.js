2cef0c229bf4257f557bb9860c1800dac257b36a
GauravBarde <gaurav.barde@thecompetenza.com>
2022-12-21 20:08:04 +0530

    Added content to exercises component

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,7 +1,26 @@
+import { Box, Stack, Typography } from "@mui/material";
 import React from "react";
+import ExerciseCard from "./ExerciseCard";
 
-const Exercises = () => {
-  return <div>Exercises</div>;
+const Exercises = ({ exercises, bodyPart, setBodyPart }) => {
+  console.log(exercises);
+  return (
+    <Box id="exercises" sx={{ mt: { lg: "110px" } }} mt="50px" p="20px">
+      <Typography variant="h3" mb="46px">
+        Showing Result
+      </Typography>
+      <Stack
+        direction="row"
+        sx={{ gap: { lg: "110px", xs: "50px" } }}
+        flexWrap="wrap"
+        justifyContent="center"
+      >
+        {exercises.map((exercise, index) => (
+          <ExerciseCard key={index} exercise={exercise} />
+        ))}
+      </Stack>
+    </Box>
+  );
 };
 
 export default Exercises;
