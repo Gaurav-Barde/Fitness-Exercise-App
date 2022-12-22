b5f3c44a8a70d6e85bbf262f4b8d93685b96dd09
Gaurav-Barde <gabarde25@gmail.com>
2022-12-22 19:06:08 +0530

    Added Pagination and fetching logic

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,9 +1,44 @@
-import { Box, Stack, Typography } from "@mui/material";
-import React from "react";
+import { Box, Pagination, Stack, Typography } from "@mui/material";
+import React, { useEffect, useRef, useState } from "react";
+import { exerciseOptions, fetchData } from "../utils/fetchData";
 import ExerciseCard from "./ExerciseCard";
 
-const Exercises = ({ exercises, bodyPart, setBodyPart }) => {
-  console.log(exercises);
+const Exercises = ({ exercises, bodyPart, setExercises }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const scrollToParent = useRef();
+
+  const exercisePerPage = 9;
+  const indexOfLastExercise = exercisePerPage * currentPage;
+  const indexOfFirstExercise = indexOfLastExercise - exercisePerPage;
+  const currentExercise = exercises.slice(
+    indexOfFirstExercise,
+    indexOfLastExercise
+  );
+
+  useEffect(() => {
+    const fetchExercises = async () => {
+      let exercisesData = [];
+      if (bodyPart === "all") {
+        exercisesData = await fetchData(
+          "https://exercisedb.p.rapidapi.com/exercises",
+          exerciseOptions
+        );
+      } else {
+        exercisesData = await fetchData(
+          `https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`,
+          exerciseOptions
+        );
+      }
+      setExercises(exercisesData);
+    };
+    fetchExercises();
+  }, [bodyPart]);
+
+  const paginate = (e, value) => {
+    setCurrentPage(value);
+    scrollToParent.current.scrollIntoView();
+  };
+
   return (
     <Box id="exercises" sx={{ mt: { lg: "110px" } }} mt="50px" p="20px">
       <Typography variant="h3" mb="46px">
@@ -14,10 +49,24 @@ const Exercises = ({ exercises, bodyPart, setBodyPart }) => {
         sx={{ gap: { lg: "110px", xs: "50px" } }}
         flexWrap="wrap"
         justifyContent="center"
+        ref={scrollToParent}
       >
-        {exercises.map((exercise, index) => (
+        {currentExercise.map((exercise, index) => (
           <ExerciseCard key={index} exercise={exercise} />
         ))}
+      </Stack>
+      <Stack mt="100px" alignItems="center">
+        {exercises.length > exercisePerPage && (
+          <Pagination
+            color="standard"
+            shape="rounded"
+            defaultPage={1}
+            count={Math.ceil(exercises.length / exercisePerPage)}
+            size="large"
+            page={currentPage}
+            onChange={paginate}
+          />
+        )}
       </Stack>
     </Box>
   );
