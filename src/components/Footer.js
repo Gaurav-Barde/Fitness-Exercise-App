dec3b6932ba7ebee04efee7c37452e306844fee4
Gaurav-Barde <gabarde25@gmail.com>
2022-12-23 21:20:44 +0530

    Added logic & jsx for the footer  page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import logo from "../assets/images/Logo-1.png";
 
 const Footer = () => {
-  return <div>Footer Component</div>;
+  return (
+    <Box mt="80px" bgcolor="#fff3f4">
+      <Stack gap="40px" px="40px" pt="24px" alignItems="center">
+        <img src={logo} alt="logo" width="200px" height="40px" />
+        <Typography variant="h5" pb="40px" mt="10px">
+          Made with ❤️ by{" "}
+          <a
+            href="https://www.youtube.com/@NeatDesigns/videos"
+            target="_blank"
+            rel="norefrer"
+            style={{ textDecoration: "none" }}
+          >
+            Gaurav Barde
+          </a>
+        </Typography>
+      </Stack>
+    </Box>
+  );
 };
 
 export default Footer;
